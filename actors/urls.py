2684d441aa3e4624e6ce0efaa0d31f3a0ba9f9c9
Alison Pereira Ribeiro <alisonrib17@gmail.com>
2023-12-26 14:52:10 -0300

    Criando app e models para actors

---
+++ actors/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+
+urlpatterns = [
+    #path('actors/', GenreCreateListView.as_view(), name='actors-create-list'),
+    #path('actors/<int:pk>', GenreDetailUpdateDeleteView.as_view(), name='actors-detail-view'),
+]
--- app/urls.py
+++ app/urls.py
@@ -22,6 +22,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('genres.urls')),
+    path('api/', include('actors.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^.*$', RedirectView.as_view(url='swagger/', permanent=False)),
 ]
