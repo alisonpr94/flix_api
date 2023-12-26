8afc387411e678ee9ab09a2c85b1d5bb1d98ca8c
Alison Pereira Ribeiro <alisonrib17@gmail.com>
2023-12-26 14:35:55 -0300

    Crud de gênero com generics class

--- genres/urls.py
+++ genres/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import genre_create_list_view, genre_detail_view
+from .views import GenreCreateListView, GenreDetailUpdateDeleteView
 
 urlpatterns = [
-    path('genres/', genre_create_list_view, name='genre-create-list'),
-    path('genres/<int:pk>', genre_detail_view, name='genre-detail-view'),
+    path('genres/', GenreCreateListView.as_view(), name='genre-create-list'),
+    path('genres/<int:pk>', GenreDetailUpdateDeleteView.as_view(), name='genre-detail-view'),
 ]
