0782954f34fd918050e24689a020467ee77457ec
Alison Pereira Ribeiro <alisonrib17@gmail.com>
2023-12-27 14:50:46 -0300

    Calculando idade dos atores

--- actors/serializers.py
+++ actors/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from .models import Actor
+from datetime import date
 
 class ActorSerializer(serializers.ModelSerializer):
+    age = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Actor
         fields = '__all__'
+
+    def get_age(self, obj):
+        today = date.today()
+        return today.year - obj.birthday.year - ((today.month, today.day) < (obj.birthday.month, obj.birthday.day))
