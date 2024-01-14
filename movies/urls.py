88198f2294a8062de546510a14048e50ced37d47
Alison Pereira Ribeiro <alisonrib17@gmail.com>
2024-01-14 17:29:25 -0300

    Criando endpoint para estatísticas

--- movies/urls.py
+++ movies/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import MovieCreateListView, MovieDetailUpdateDeleteView
+from .views import MovieCreateListView, MovieDetailUpdateDeleteView, MovieStatsView
 
 urlpatterns = [
     path('movies/', MovieCreateListView.as_view(), name='movie-create-list'),
     path('movies/<int:pk>', MovieDetailUpdateDeleteView.as_view(), name='movie-detail-view'),
+    path('movies/stats/', MovieStatsView.as_view(), name='movie-stats-view'),
 ]
